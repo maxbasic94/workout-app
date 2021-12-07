ad8af93d24d8b79e4682392d1822be5ed64aa698
Maksim Ashmiana <max.basic.94@gmai.com>
2021-12-07 14:33:35 +0300

    recovery point

--- src/startPage/exercisesList/ExersicesList.tsx
+++ src/startPage/exercisesList/ExersicesList.tsx
@@ -4,14 +4,12 @@ import ExerciseView from '../exerciseView/ExerciseView';
 
 const ExercisesList: React.FunctionComponent = (): JSX.Element => {
   const [result, setResult] = useState<DataType | null>(null);
-
+  const apiPath: string = process.env.REACT_APP_API_PATH || '';
   useEffect(() => {
-    fetch(
-      'https://rnd.kilohealthservices.com/api/quizzes/workouts?api_token=4bfcebd0-0216-4572-bdb7-939e9600b9b2'
-    )
+    fetch(`${apiPath}`)
       .then((res) => res.json())
       .then((data) => setResult(data.data));
-  }, []);
+  }, [apiPath]);
 
   return (
     <div className="exersisesList">
