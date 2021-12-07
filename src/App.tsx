246c8a0e77c758514acadfcb639e3a55193121be
Maksim Ashmiana <max.basic.94@gmai.com>
2021-12-07 15:50:40 +0300

    added favicon.ico and react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import StartPage from './startPage/StartPage';
+import ExercisePage from './exercisePage/ExercisePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 const App: React.FunctionComponent = (): JSX.Element => {
   return (
     <div className="app">
-      <StartPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<StartPage />} />
+          <Route path="/exercise" element={<ExercisePage />} />
+          <Route index element={<StartPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/exercisePage/ExercisePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ExercisePage: React.FC = (): JSX.Element => {
+  return <div className="exercisePage">Exercise Page</div>;
+};
+
+export default ExercisePage;
