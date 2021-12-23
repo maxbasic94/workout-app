a95c219120403ce6149a2c0f873a04d539e66b91
Maksim Ashmiana <max.basic.94@gmai.com>
2021-12-23 15:35:36 +0300

    edit styles to StartButton

--- src/startPage/startButton/StartButton.tsx
+++ src/startPage/startButton/StartButton.tsx
@@ -3,7 +3,7 @@ import './StartButton.css';
 
 const StartButton: React.FunctionComponent = (): JSX.Element => {
   return (
-    <a href="/exercise" className="StartPage-StartInfo_button_start">
+    <a href="/exercise" className="StartPage-StartButton_button_start">
       <span>Start Workout</span>
     </a>
   );
