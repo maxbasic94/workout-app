c927064f398ef48f4f9f8c0961d9d78c394926e4
Maksim Ashmiana <max.basic.94@gmai.com>
2021-12-23 15:00:12 +0300

    add BEM for NotFoundPage

--- src/notFoundPage/NotFoundPage.tsx
+++ src/notFoundPage/NotFoundPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NotFoundPage: React.FC = (): JSX.Element => {
-  return <div className="notFoundPage">Page is not found</div>;
+  return <div className="NotFoundPage">Page is not found</div>;
 };
 
 export default NotFoundPage;
