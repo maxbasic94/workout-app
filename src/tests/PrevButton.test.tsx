58de6e42f72b2015b644f5046180e3d08213639a
Maksim Ashmiana <max.basic.94@gmai.com>
2021-12-24 20:09:14 +0300

    added disabled test to Prev button

---
+++ src/tests/PrevButton.test.tsx
@@ -0,0 +1,28 @@
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import PrevButton from '../exercisePage/buttons/PrevButton';
+jest.mock('./images/prevExr.png');
+
+let container: Element;
+beforeAll(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterAll(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+});
+
+it('test disabled Next button', () => {
+  act(() => {
+    render(<PrevButton index={0} />, container);
+  });
+  const button: HTMLButtonElement | null = container.querySelector('.ExersicePage_button_prev');
+  expect(button?.disabled).toBe(true);
+
+  act(() => {
+    render(<PrevButton index={10} />, container);
+  });
+  expect(button?.disabled).toBe(false);
+});
