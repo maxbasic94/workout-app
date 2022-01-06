87b626ce7e024006638792e39c94342d498a2c62
Maksim Ashmiana <max.basic.94@gmai.com>
2022-01-06 11:40:50 +0300

    refactored code

--- src/pages/exercisePage/finishWorkout/FinishWorkout.tsx
+++ src/pages/exercisePage/finishWorkout/FinishWorkout.tsx
@@ -10,15 +10,15 @@ interface FinishWorkoutProps {
 const FinishWorkout: React.FC<FinishWorkoutProps> = ({ time }): JSX.Element => {
   return (
     <div className="ExercisePage-FinishWorkout_container">
-      <img className="ExercisePage-FinishWorkout_image" src={complete} />
+      <img className="ExercisePage-FinishWorkout_image" src={complete} alt="complete" />
       <div className="ExercisePage-FinishWorkout_complete">Workout completed!</div>
       <div className="ExercisePage-FinishWorkout_description">
-        Nice job. You’re done. Here’s the workout summary.
+        Nice job. You&#x2019;re done. Here&#x2019;s the workout summary.
       </div>
       <div className="ExercisePage-FinishWorkout_text_timeCaption">Minutes</div>
       <div className="ExercisePage-FinishWorkout_text_time">{Math.round(time / 60)}</div>
       <Link className="ExercisePage-FinishWorkout_button_end" to={'/'}>
-        Save & Continue
+        Save &#38; Continue
       </Link>
     </div>
   );
