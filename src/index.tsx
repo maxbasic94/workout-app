8814ddb070a7a74df32464fc4cd2aac51b11c7ad
Maksim Ashmiana <max.basic.94@gmai.com>
2022-01-24 16:15:26 +0300

    refactored code

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
-import './Index.css';
+import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
